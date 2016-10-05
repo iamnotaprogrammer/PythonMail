fcf0aac021e64d6cf9c7d8010308d5157fcb8bef
Ivan <i.smirnov@contentus.info>
2016-10-05 13:08:15 +0300

    handle negative total value

--- first_homework/myfinance.py
+++ first_homework/myfinance.py
@@ -5,19 +5,33 @@ class FormatException(Exception):
     pass
 
 
+def my_round(f):
+    def wrapper(value):
+        return f(round(value, 2))
+    return wrapper
+
+
+@my_round
+def printer(val):
+    print val
+
+
 class Charge(object):
    
     def __init__(self, value):
         try:
-            self._value = round(float(value))
+            self._value = round(float(value), 2)
         except:
             raise FormatException('incorrect charge format')
     
+    def __str__(self):
+        return str(self.value)
+
     value = property()
 
     @value.setter
     def value(self, value):
-        self._value = round(float(value))
+        self._value = round(float(value), 2)
 
     @value.getter
     def value(self):
@@ -30,33 +44,38 @@ class Charge(object):
 
 class Account(object):
     def __init__(self, charge):
-        self.charges = [charge,]
-        self.total = charge.value
-    
-    def print_completed_charges(self):
-        values = [str(charge.value) for charge in self.charges]
-        print 'completed charges {0}'.format(' , '.join(values))
-    
-    def get_total(self):
-        return self.total
+        self._charges = [charge,]
+        self._total = charge.value
+
+    def __str__(self):
+        return "Account's Charges: {0}\n Total : {1}".format(', '.join(map(str, self.charges)), self.total)
+
+    def __iter__(self):
+        return iter(self.charges)
+
+    @property
+    def total(self):
+        return self._total
+
+    @property
+    def charges(self):
+        return self._charges
 
     def add_trunsaction(self, charge):
-        self.charges.append(charge)
-        self.total += charge.value
+        self._charges.append(charge)
+        self._total = max(self._total + charge.value, 0)
+
 
 if __name__ == '__main__':
     val = raw_input()
     charge = Charge(val)
     acc = Account(charge)
+    print acc    
     while True:
-        # try:
-            val = raw_input()
-            charge = Charge(val)
-            acc.add_trunsaction(charge)
-            acc.print_completed_charges()
-            print acc.get_total()
-        # except Exception as e:
-        #     print str(e)
+        val = raw_input()
+        charge = Charge(val)
+        acc.add_trunsaction(charge)
+        print acc
 
 
 
